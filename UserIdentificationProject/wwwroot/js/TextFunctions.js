76bdb061b8645732c9332c1181183796261bf6ba
Hubert Zając <hubert.zajac98@gmail.com>
2021-07-07 15:46:13 +0200

    working table

--- UserIdentificationProject/wwwroot/js/TextFunctions.js
+++ UserIdentificationProject/wwwroot/js/TextFunctions.js
@@ -10,30 +10,39 @@ function countChar(val) {
 
 var start = 0;
 var counter = 0;
+var delta = 0;
 function processKeyDown(e) {
     if (!start) {
         start = (new Date()).getTime();
     }
     ++counter;
-    document.getElementById("counter").innerHTML = "Consecutive keyDown events: " + cnt +"\n";
+    document.getElementById("counter").innerHTML = "Consecutive keyDown events: " + counter + "\n";
+    if (counter > 1) {
+
+    }
 };
 
 function processKeyUp(e) {
     if (start != 0) {
-        var delta = (new Date()).getTime() - start;
+        delta = (new Date()).getTime() - start;
         document.getElementById("time").innerHTML = "Time between first keyDown and keyUp: " + delta + " ms\n";
-        dynamicTable();
     }
 
     start = 0;
-    counter = 0;
+
+    if (counter != 0) {
+        addRowCell();
+    }
+    
 };
 
-function dynamicTable() {
+function addRowCell() {
     var table = document.getElementById("myTable");
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
-    cell1.innerHTML = "NEW CELL1";
-    cell2.innerHTML = "NEW CELL2";
+    cell1.innerHTML = counter + "";
+    cell2.innerHTML = delta;
+
+    counter = 0;
 }
