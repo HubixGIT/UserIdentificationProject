bc7561a6cadcc763c687ebd811ae494c6533d48d
hzajac <hzajac@PCHZAJAC>
2021-07-07 10:05:57 +0200

    Add project files.

---
+++ UserIdentificationProject/Models/HomeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserIdentificationProject.Models
+{
+    public class HomeModel
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public int Time { get; set; }
+    }
+}
---
+++ UserIdentificationProject/wwwroot/js/TextFunctions.js
@@ -0,0 +1,39 @@
+﻿
+function countChar(val) {
+    var len = val.value.length;
+    if (len >= 151) {
+        val.value = val.value.substring(0, 150);
+    } else {
+        $('.numbersofChar').text(150 - len);
+    }
+};
+
+var start = 0;
+var counter = 0;
+function processKeyDown(e) {
+    if (!start) {
+        start = (new Date()).getTime();
+    }
+    ++counter;
+    document.getElementById("counter").innerHTML = "Consecutive keyDown events: " + cnt +"\n";
+};
+
+function processKeyUp(e) {
+    if (start != 0) {
+        var delta = (new Date()).getTime() - start;
+        document.getElementById("time").innerHTML = "Time between first keyDown and keyUp: " + delta + " ms\n";
+        dynamicTable();
+    }
+
+    start = 0;
+    counter = 0;
+};
+
+function dynamicTable() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "NEW CELL1";
+    cell2.innerHTML = "NEW CELL2";
+}
