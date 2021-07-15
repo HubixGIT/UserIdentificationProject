6e48cd841de2864bf34052a28dede8238a1527c0
Hubert Zając <hubert.zajac98@gmail.com>
2021-07-15 14:49:05 +0200

    TO DO: Repeat

--- UserIdentificationProject/wwwroot/js/TextFunctions.js
+++ UserIdentificationProject/wwwroot/js/TextFunctions.js
@@ -1,5 +1,4 @@
 ﻿var startTime = (new Date()).getTime();
-var pressTime = 0;
 var keys = 0;
 var repeat = false;
 var arr = [];
@@ -22,50 +21,45 @@ txt.addEventListener('keydown', event => {
         nextKeyTimeArr[keyCode] = nextKeyTime;
         startTimeArr[keyCode] = startTime;
     }
-    //else {
-    //    if (!repeat) {
-    //        startTime = (new Date()).getTime();
-    //    }
-    //    repeat = true;
-    //    keys = event.code;
-    //    pressTime = null;
+    else {
+        if (!repeat) {
+            startTime = (new Date()).getTime();
+        }
+        repeat = true;
+        keys = event.code;
+        var pressTime = null;
 
-    //    log.textContent += `${keys}        `;
-    //    ms.textContent += `${pressTime}        `;   
-    //}
+        log.textContent += `${keys}        `;
+        ms.textContent += `${pressTime}        `;   
+    }
 });
 
 txt.addEventListener('keyup', event => {
-    //if (repeat) {
-    //    pressTime = ((new Date()).getTime() - startTime);
+    if (repeat) {
+        pressTime = ((new Date()).getTime() - startTime);
 
-    //    log.textContent += `${keys}        `;
-    //    ms.textContent += `${pressTime}        `;
+        log.textContent += `${keys}        `;
+        ms.textContent += `${pressTime}        `;
 
-    //    repeat = false;
-    //    start = 0;
-    //}
-    //else {
+        repeat = false;
+        start = 0;
+    }
+    else {
         var ut = (new Date()).getTime();
+        var key = event.code;
         var keyCode = event.keyCode;
         var pressTime = 0;
         var nextKeyTime = 0;
         if (arr[keyCode] == 1) {
             pressTime = ut - startTimeArr[keyCode];
             nextKeyTime = nextKeyTimeArr[keyCode];
-            dataArr = { keyCode, nextKeyTime, pressTime };
+            dataArr = { key, keyCode, nextKeyTime, pressTime };
         }
-        //keys = event.code;
-        //pressTime = ((new Date()).getTime() - startTime);
-
-        //log.textContent += `${keys}        `;
-        //msTime.textContent += `${pressTime}        `;
-    //}
+    }
 });
 
 function showArray() {
     document.getElementById("arrPrint2").innerHTML = JSON.stringify(dataArr);
-    //document.getElementById("arrPrint").innerHTML = JSON.stringify(time);  
 };
 
 function countChar(val) {
